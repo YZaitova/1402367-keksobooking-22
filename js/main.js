ce752fe9c061b64415e4714ae6194066acc8e02a
YZaitova <zaityana24@gmail.com>
2021-02-15 03:24:04 +0700

    Добавляет функции

---
+++ js/main.js
@@ -0,0 +1,28 @@
+let randomIntInclusive = function get(min, max) {
+  if (min || max <= 0) {
+    alert('Введен некорректный диапазон. Введите значения больше либо равное 0');
+  }
+  if (max <= min) {
+    let container = max;
+    max = min;
+    min = container;
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+randomIntInclusive(1, 45);
+let geographicalСoordinates = function(min, max, NumberOfSimbolsAfterComma) {
+  if (min || max <= 0) {
+    alert('Введен некорректный диапазон. Введите значения больше либо равное 0');
+  }
+  if (max <= min) {
+    let container = max;
+    max = min;
+    min = container;
+  }
+  let randomNumber = Math.random() * (max - min + 1) + min;
+  randomNumber.toFixed(NumberOfSimbolsAfterComma);
+  return randomNumber;
+};
+geographicalСoordinates(5.5, 8.25, 3);
