dec4296f2b3941c9e685af78a6cc3ddfed9ac919
YZaitova <zaityana24@gmail.com>
2021-02-28 01:17:20 +0700

    Пытается сделать служебные функции по комментарию наставника

--- js/main.js
+++ js/main.js
@@ -1,27 +1,29 @@
-let randomIntInclusive = function get(min, max) {
-  if (min || max <= 0) {
-    alert('Введен некорректный диапазон. Введите значения больше либо равное 0');
+const checksRange = function (min, max) {
+  if (min || (max <= 0)) {
+    return alert('Введен некорректный диапазон. Введите значения больше либо равное 0');
   }
+};
+
+const checksMinLargerThanMax = function (min, max) {
   if (max <= min) {
     let container = max;
     max = min;
     min = container;
   }
+};
+
+let randomIntInclusive = function get(min, max) {
+  checksRange(min, max);
+  checksMinLargerThanMax (min, max);
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
-randomIntInclusive(1, 45);
+randomIntInclusive(5, 7);
 
 let geographicalСoordinates = function(min, max, NumberOfSimbolsAfterComma) {
-  if (min || max <= 0) {
-    alert('Введен некорректный диапазон. Введите значения больше либо равное 0');
-  }
-  if (max <= min) {
-    let container = max;
-    max = min;
-    min = container;
-  }
+  checksRange(min, max);
+  checksMinLargerThanMax (min, max);
   let randomNumber = Math.random() * (max - min + 1) + min;
   randomNumber = +randomNumber.toFixed(NumberOfSimbolsAfterComma);
   return randomNumber;
